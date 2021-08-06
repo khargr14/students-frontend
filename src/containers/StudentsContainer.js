61f8864317a9ec67202ae4e37f565cb7e235b95c
khargr14 <kay_hargrove@yahoo.com>
2021-08-06 07:45:39 -0400

    update studentcontainer

---
+++ src/containers/StudentsContainer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
+import Students from '../components/Students'
+import StudentForm from '../components/StudentForm'
+import { fetchStudents } from '../actions/fetchStudents'
+import Student from '../components/Student'
+import StudentFormEdit from '../components/StudentFormEdit'
+
+//stateful class container
+
+class StudentContainer extends React.Component {
+
+    componentDidMount() {
+        console.log("a")
+        this.props.fetchStudents();
+        console.log("b")
+    }
+
+  
+    render() {
+        console.log('this biz props==>>', this.props)
+        return (
+            <div>
+                <Switch>
+                    <Route path='/students/new' component={StudentForm} />
+                    <Route path='/students/:id/edit' render={(routerProps) => <StudentFormEdit {...routerProps} students={this.props.students} />} />
+                    <Route path='/students/:id' render={(routerProps) => <Student {...routerProps} students={this.props.students} />} />
+                    <Route path='/students' render={(routerProps) => <Students {...routerProps} students={this.props.students} />} />
+                </Switch>
+            </div>
+        )
+    }
+}
+// <Students {...routerProps} students={this.props.students}  storing data/>
+const mapStateToProps = (state) => {
+    return {
+        students: state.students,
+    };
+};
+
+export default connect(mapStateToProps, { fetchStudents })(StudentContainer);
