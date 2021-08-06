8d4bc678c810dada3bd5217458b4db359455c30b
khargr14 <kay_hargrove@yahoo.com>
2021-08-06 08:08:42 -0400

    update index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import studentsReducer from './reducers/studentReducer'
 
 import Home from './Home';
+import Students from './containers/StudentsContainer';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//window.__REDUX_DEVTOOLS_EXTENSION__ / window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+
+let store = createStore(studentReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <Container>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/students" component={Students} />
+        </Switch>
+        <Footer/>
+      </Container>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
